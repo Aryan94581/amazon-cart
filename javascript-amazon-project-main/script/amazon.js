ecd4cc6a48ad101c2746daf1e48c3ac1cfdf7ddd
AryanSwami <swamiaryan611@gmail.com>
2024-02-24 19:15:16 +0530

    a

--- javascript-amazon-project-main/script/amazon.js
+++ javascript-amazon-project-main/script/amazon.js
@@ -1,1 +1,29 @@
-console.log("hello");
+const products = [
+  {
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      star: 4.5,
+      count: 87,
+    },
+    priceCent: 1099,
+  },
+  {
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      star: 4.0,
+      count: 127,
+    },
+    priceCent: 2095,
+  },
+  {
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      star: 4.5,
+      count: 56,
+    },
+    priceCent: 7099,
+  },
+];
