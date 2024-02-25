97085d6b8edbf7ae9250fda53c9bdcfa310a76e3
AryanSwami <swamiaryan611@gmail.com>
2024-02-25 20:28:37 +0530

    save cart in local storage

--- javascript-amazon-project-main/script/checkout.js
+++ javascript-amazon-project-main/script/checkout.js
@@ -13,7 +13,8 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummryhtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -95,6 +96,7 @@ document.querySelectorAll(".js-delete-quantity-link").forEach((link) => {
   link.addEventListener("click", () => {
     let productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+
+    document.querySelector(`.js-cart-item-container-${productId}`).remove();
   });
 });
